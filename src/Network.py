704eb70dbe523b22dc9bbcd91bb7e86823664736
kebal <kebal.badal555@gmail.com>
2025-07-23 13:11:03 +0545

    Created Initialization Method

---
+++ src/Network.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Network(object):
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        
+        # Initialize biases as a list
+        self.biases = []
+        for y in sizes[1:]:  # for every layer except the input layer
+            b = np.random.randn(y, 1)  # column vector of biases
+            self.biases.append(b)
+        
+        # Initialize weights as a list
+        self.weights = []
+        for x, y in zip(sizes[:-1], sizes[1:]):  # pairs of consecutive layers
+            w = np.random.randn(y, x)  # weight matrix with shape (y, x)
+            self.weights.append(w)
